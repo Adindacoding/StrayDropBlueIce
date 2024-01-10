f517da2e2d7b5ac987986351b6545dfef9eb38a5
adindacoding <adindabeer@gmail.com>
2024-01-10 15:34:15 +0100

    client fixed

--- src/main/java/net/adinda/straydropice/StrayDropIce.java
+++ src/main/java/net/adinda/straydropice/StrayDropIce.java
@@ -7,7 +7,8 @@ import org.slf4j.LoggerFactory;
 
 public class StrayDropIce implements ModInitializer {
 	public static final String MOD_ID = "straydropice";
-	public static final Logger LOGGER = LoggerFactory.getLogger("MOD_ID");
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
 
 	@Override
 	public void onInitialize() {
---
+++ src/main/java/net/adinda/straydropice/StrayDropIceClient.java
@@ -0,0 +1,10 @@
+package net.adinda.straydropice;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class StrayDropIceClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
