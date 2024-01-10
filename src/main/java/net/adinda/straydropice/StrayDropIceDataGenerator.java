fe876432d70263f0146c1e91e662232686e97524
adindacoding <adindabeer@gmail.com>
2024-01-10 14:57:27 +0100

    Initial commit

---
+++ src/main/java/net/adinda/straydropice/StrayDropIceDataGenerator.java
@@ -0,0 +1,11 @@
+package net.adinda.straydropice;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class StrayDropIceDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
